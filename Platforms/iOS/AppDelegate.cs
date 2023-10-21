162a4d98cc7207bbe156694b324ae5866a39a313
andmalt <andrea.maltempi@gmail.com>
2023-10-21 15:09:16 +0200

    Aggiungere i file di progetto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BoolBnB_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
