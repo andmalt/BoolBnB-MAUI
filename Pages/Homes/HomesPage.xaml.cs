1b18f0f5139422e28f7cacfc9355c961031f16f8
andmalt <andrea.maltempi@gmail.com>
2024-03-16 14:26:05 +0100

    added viewmodel in the homesPage

--- Pages/Homes/HomesPage.xaml.cs
+++ Pages/Homes/HomesPage.xaml.cs
@@ -1,104 +1,11 @@
-using BoolBnB_MAUI.Data.Entites;
-using BoolBnB_MAUI.Data.Guest;
-using BoolBnB_MAUI.Pages.Home;
-using BoolBnB_MAUI.Services;
-using BoolBnB_MAUI.Services.Interface;
-using CommunityToolkit.Maui.Core.Extensions;
-using System.Windows.Input;
+using BoolBnB_MAUI.ViewModels;
 
 namespace BoolBnB_MAUI.Pages.Homes;
 
 public partial class HomesPage : ContentPage
 {
-	private readonly IHttpService _httpService;
-	private readonly AuthService _authService;
-    private bool isAuth;
-    public ICommand ItemTappedCommand { get; }
-    public bool IsAuth
-    {
-        get => isAuth;
-        set
-        {
-            isAuth = value;
-            OnPropertyChanged(nameof(IsAuth));
-            EnableBinding();
-        }
-    }
-
     public HomesPage()
 	{
 		InitializeComponent();
-		_httpService = new HttpService();
-        _authService = new AuthService();
-        ItemTappedCommand = new Command<Apartment>(OnItemSelected);
-    }
-
-    private void EnableBinding()
-    {
-        BindingContext = this;
-    }
-
-    protected async override void OnAppearing()
-	{
-		base.OnAppearing();
-
-        if (await _authService.IsAuthenticatedAsync())
-        {
-            IsAuth = true;
-        }
-        else
-        {
-            IsAuth = false;
-        }
-
-        menuIcon.IsVisible = false;
-        housesList.IsVisible = false;
-        loadingIndicator.IsRunning = true;
-        loadingIndicator.IsVisible = true;
-        var response = await GetApartmentsAsync();
-        if (response.Success)
-        {
-            housesList.ItemsSource = response.Apartments.Data.ToObservableCollection(); ;
-            housesList.IsVisible = true;
-        }        
-        loadingIndicator.IsRunning = false;
-        loadingIndicator.IsVisible = false;
-        menuIcon.IsVisible = true;
-    }
-
-	protected async Task<HousesResponse> GetApartmentsAsync()
-	{
-		try
-		{
-            var response = await _httpService.Get<HousesResponse>("/api/homes-mobile");
-            if (response.Success)
-            {
-                return response;
-            }
-            else
-            {
-                return new HousesResponse { Success = false, Apartments = { } };
-            }
-        }
-        catch (HttpRequestException ex)
-		{
-            await DisplayAlert("Errore", "Imposibile ottenere dati, connessione internet mancante.", "Ok");
-            Console.WriteLine($"ERROR (getApartments):{ex.Message}");
-            throw;
-        }
-
-    }
-
-    private async void OnItemSelected(Apartment apartment)
-    {
-        //Console.WriteLine($"Selected Object: {e.SelectedItem}");
-        if (apartment != null)
-        {
-            var parameters = new Dictionary<string, object>();
-            parameters.Add("Apartment", apartment);
-            await Shell.Current.GoToAsync($"{nameof(HomePage)}", true, parameters);
-        }
-        // Deselect item
-        housesList.SelectedItem = null;
     }
 }
---
+++ ViewModels/HomesViewModel.cs
@@ -0,0 +1,116 @@
+﻿using BoolBnB_MAUI.Data.Entites;
+using BoolBnB_MAUI.Data.Guest;
+using BoolBnB_MAUI.Pages.Home;
+using BoolBnB_MAUI.Services;
+using BoolBnB_MAUI.Services.Interface;
+using CommunityToolkit.Maui.Core.Extensions;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+
+namespace BoolBnB_MAUI.ViewModels
+{
+    public partial class HomesViewModel : ObservableObject
+    {
+        private readonly IHttpService _httpService;
+        private readonly AuthService _authService;
+
+        [ObservableProperty]
+        public ObservableCollection<Apartment> apartments;
+
+        [ObservableProperty]
+        public bool isAuth;
+
+        [ObservableProperty]
+        public bool isVisibleLoading;
+
+        [ObservableProperty]
+        public bool isVisibleMenu;
+
+        [ObservableProperty]
+        public bool isVisibleList;
+
+        public HomesViewModel()
+        {
+            _httpService = new HttpService();
+            _authService = new AuthService();
+        }
+
+        [RelayCommand]
+        public async Task Appearing()
+        {
+            try
+            {
+                if (await _authService.IsAuthenticatedAsync())
+                {
+                    IsAuth = true;
+                }
+                else
+                {
+                    IsAuth = false;
+                }
+
+                IsVisibleMenu = false;
+                IsVisibleList = false;
+                IsVisibleLoading = true;
+                var response = await GetApartmentsAsync();
+                if (response.Success)
+                {
+                    Apartments = response.Apartments.Data.ToObservableCollection(); ;
+                    IsVisibleList = true;
+                }
+                IsVisibleLoading = false;
+                IsVisibleMenu = true;
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        [RelayCommand]
+        public async Task ItemSelected(Apartment apartment)
+        {
+            //Console.WriteLine($"Selected Object: {e.SelectedItem}");
+            if (apartment != null)
+            {
+                var parameters = new Dictionary<string, object>();
+                parameters.Add("Apartment", apartment);
+                await Shell.Current.GoToAsync($"///{nameof(HomePage)}", true, parameters);
+            }
+            // Deselect item
+            //housesList.SelectedItem = null;
+        }
+
+        private async Task<HousesResponse> GetApartmentsAsync()
+        {
+            try
+            {
+                var response = await _httpService.Get<HousesResponse>("/api/homes-mobile");
+                if (response.Success)
+                {
+                    return response;
+                }
+                else
+                {
+                    return new HousesResponse { Success = false, Apartments = { } };
+                }
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"JSON deserialization error: {ex.Message}");
+                // Handle invalid JSON
+                return null;
+            }
+            catch (HttpRequestException ex)
+            {
+                //DisplayAlert("Errore", "Imposibile ottenere dati, connessione internet mancante.", "Ok");
+                Console.WriteLine($"ERROR (getApartments):{ex.Message}");
+                throw;
+            }
+
+        }
+    }
+}
+ 
